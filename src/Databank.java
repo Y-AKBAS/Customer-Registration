e80e31e47ff1e09bca1a17bea68da78489b85eb0
yasin <yasin.akbas@criptext.com>
2022-11-21 17:59:12 +0100

    clear bin

--- src/Databank.java
+++ src/Databank.java
@@ -1,116 +1,137 @@
-
-import java.sql.*;
-import java.time.LocalDate;
-
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
-public class Databank 
-{
-	private final String url = "jdbc:postgresql://localhost/customerregistration";
-    private final String user = "postgres";
-    private final String password = "<Password>";
-    private Connection con=null;
-	
-	public Connection connect()
-	{
-		try
-		{
-			con=DriverManager.getConnection(url,user,password);
-			
-		}catch (SQLException e) { System.out.println(e.getMessage());}
-		
-		return con;
-	}
-	
-	public void insertion(TextField fname,TextField lname,TextField city,TextField pnumber,TextField email,DatePicker dp) throws SQLException
-	{
-	   String in="INSERT INTO CustomerInfo (FirstName,LastName,City,PhoneNumber,Email,DateOfReg) VALUES (?,?,?,?,?,?)";
-	   PreparedStatement pstm=con.prepareStatement(in);
-	   	     
-	     String FirstName=fname.getText().trim();
-	     String LastName=lname.getText().trim();
-	     String City=city.getText().trim();
-	     String PhoneNumber=pnumber.getText().trim();
-	     String Email=email.getText().trim();
-	     
-	     LocalDate ld=dp.getValue();
-	     Date d=Date.valueOf(ld);
-	     
-	     pstm.setString(1, FirstName);
-	     pstm.setString(2, LastName);
-	     pstm.setString(3, City);
-	     pstm.setString(4, PhoneNumber);
-	     pstm.setString(5, Email);
-	     pstm.setDate(6,d);
-	     
-	     pstm.executeUpdate();
-	}
-	
-	public void delete(TextField tf) throws Exception
-	{
-		String de="DELETE FROM CustomerInfo where customerid=?";
-		
-		PreparedStatement pstm=con.prepareStatement(de);
-		pstm.setInt(1,Integer.parseInt(tf.getText()));
-		
-		pstm.execute();
-	}
-	
-	public void search(TextField cid,TextField fname,TextField lname,TextField city,TextField pnumber,TextField email,DatePicker dp) throws Exception
-	{
-		String se="SELECT * FROM CustomerInfo where customerid=?";
-		
-		PreparedStatement pstm=con.prepareStatement(se);
-		pstm.setInt(1, Integer.parseInt(cid.getText()));
-		
-		ResultSet rs=pstm.executeQuery();
-		
-		LocalDate ld;
-		
-		while(rs.next())
-		{
-			 fname.setText(rs.getString("firstname").trim());
-		     lname.setText(rs.getString("lastname").trim());
-		     city.setText(rs.getString("city").trim());
-		     pnumber.setText(rs.getString("phonenumber").trim());
-		     email.setText(rs.getString("email").trim());
-		     ld=rs.getDate("dateofreg").toLocalDate();
-			 dp=new DatePicker(ld);
-		}
-	}
-	
-	public void update(TextField cid,TextField fname,TextField lname,TextField city,TextField pnumber,TextField email,DatePicker dp) throws Exception
-	{
-		PreparedStatement pstm;
-		String str;
-		
-		str="UPDATE CustomerInfo SET firstname=?,lastname=?,city=?,phonenumber=?,email=?,dateofreg=?"
-				    + " where customerid="+Integer.valueOf(cid.getText());
-		pstm=con.prepareStatement(str);
-		pstm.setString(1,fname.getText());
-		pstm.setString(2, lname.getText());
-		pstm.setString(3, city.getText());
-		pstm.setString(4, pnumber.getText());
-		pstm.setString(5, email.getText());
-		
-		LocalDate ld=dp.getValue();
-		Date d=Date.valueOf(ld);
-		pstm.setDate(6,d);
-		
-		pstm.executeQuery();
-	}	
-	
-	public static void main(String args[])
-	{
-		Databank db=new Databank();
-		Connection conn=db.connect();
-		
-		try 
-		{
-			conn.close();	
-		}
-		catch(SQLException e) {}
-		
-	}
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DBHandler {
+    private static final String DB_CONNECTION_URL = "jdbc:postgresql://localhost/customerregistration";
+    private static final String DB_USER_NAME = "postgres";
+    private static final String DB_PASSWORD = "...";
+
+    private static final String INSERTION = "INSERT INTO CustomerInfo (FirstName,LastName,City,PhoneNumber,Email,DateOfReg)" +
+            " VALUES (?,?,?,?,?,?)";
+    private static final String DELETION = "DELETE FROM CustomerInfo where customerid=?";
+    private static final String SEARCH = "SELECT * FROM CustomerInfo where customerid=?";
+    private static final String UPDATE = "UPDATE CustomerInfo SET firstname=?,lastname=?,city=?,phonenumber=?,email=?,dateofreg=?"
+            + " where customerid=";
+
+    private static Connection DB_CONNECTION;
+
+    static {
+        try {
+            DB_CONNECTION = DriverManager.getConnection(DB_CONNECTION_URL, DB_USER_NAME, DB_PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        if (Objects.isNull(DB_CONNECTION)) {
+            DB_CONNECTION = DriverManager.getConnection(DB_CONNECTION_URL, DB_USER_NAME, DB_PASSWORD);
+        }
+        return DB_CONNECTION;
+    }
+
+    public void insertion(TextField fname, TextField lname, TextField city, TextField pnumber, TextField email, DatePicker dp) throws SQLException {
+
+        try (PreparedStatement pstm = getConnection().prepareStatement(INSERTION)) {
+
+            String FirstName = fname.getText().trim();
+            String LastName = lname.getText().trim();
+            String City = city.getText().trim();
+            String PhoneNumber = pnumber.getText().trim();
+            String Email = email.getText().trim();
+
+            LocalDate ld = dp.getValue();
+            Date d = Date.valueOf(ld);
+
+            pstm.setString(1, FirstName);
+            pstm.setString(2, LastName);
+            pstm.setString(3, City);
+            pstm.setString(4, PhoneNumber);
+            pstm.setString(5, Email);
+            pstm.setDate(6, d);
+
+            pstm.executeUpdate();
+        }
+    }
+
+    public void delete(TextField tf) throws Exception {
+        String input = tf.getText();
+
+        String regex = "[+-]?[0-9]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find() && matcher.group().equals(input)) {
+            try (PreparedStatement pstm = getConnection().prepareStatement(DELETION)) {
+                pstm.setInt(1, Integer.parseInt(tf.getText()));
+                pstm.execute();
+            }
+        }
+
+    }
+
+    public void search(TextField cid, TextField fname, TextField lname, TextField city, TextField pnumber,
+                       TextField email) throws Exception {
+
+        String input = cid.getText();
+        String regex = "[+-]?[0-9]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find() && matcher.group().equals(input)) {
+
+            ResultSet rs;
+            try (PreparedStatement pstm = getConnection().prepareStatement(SEARCH)) {
+                pstm.setInt(1, Integer.parseInt(cid.getText()));
+                rs = pstm.executeQuery();
+            }
+
+            LocalDate ld;
+            if (Objects.isNull(rs)) {
+                return;
+            }
+
+            while (rs.next()) {
+                fname.setText(rs.getString("firstname").trim());
+                lname.setText(rs.getString("lastname").trim());
+                city.setText(rs.getString("city").trim());
+                pnumber.setText(rs.getString("phonenumber").trim());
+                email.setText(rs.getString("email").trim());
+                ld = rs.getDate("dateofreg").toLocalDate();
+                DatePicker dp = new DatePicker(ld);
+            }
+        }
+    }
+
+    public void update(TextField cid, TextField fname, TextField lname, TextField city, TextField pnumber,
+                       TextField email, DatePicker dp) throws Exception {
+
+        String input = cid.getText();
+        String regex = "[+-]?[0-9]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find() && matcher.group().equals(input)) {
+            String query = UPDATE + Integer.valueOf(cid.getText());
+            try (PreparedStatement pstm = getConnection().prepareStatement(query)) {
+                pstm.setString(1, fname.getText());
+                pstm.setString(2, lname.getText());
+                pstm.setString(3, city.getText());
+                pstm.setString(4, pnumber.getText());
+                pstm.setString(5, email.getText());
+
+                LocalDate ld = dp.getValue();
+                Date d = Date.valueOf(ld);
+                pstm.setDate(6, d);
+
+                pstm.executeQuery();
+            }
+        }
+    }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,205 +1,257 @@
-
-import java.sql.*;
-import java.time.LocalDate;
-
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.layout.*;
-import javafx.geometry.*;
+import javafx.stage.Stage;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 
 public class MyFrame extends Application {
 
-	Button add,delete,update,save,search;
-	Label cid,fname,lname,city,pnumber,email,dateofreg;
-	TextField tcid,tfname,tlname,tcity,tpnumber,temail,tdateofreg;
-	DatePicker dp=new DatePicker(LocalDate.now());
-	private Databank db=new Databank();
-    private Connection connection=db.connect();
-	
-    public void setEmpty(TextField c,TextField fname,TextField lname,TextField city,TextField pnumber,TextField email,DatePicker d)
-    {
-    	c.setText("");
-    	fname.setText("");
-    	lname.setText("");
-    	city.setText("");
-    	pnumber.setText("");
-    	email.setText("");
-    	dp=new DatePicker(LocalDate.now());
+    private TextField tcid, tfname, tlname, tcity, tpnumber, temail, tdateofreg;
+    private DatePicker datePicker = new DatePicker(LocalDate.now());
+    private static final Font FONT = Font.font("Arial", FontWeight.NORMAL, FontPosture.REGULAR, 15);
+
+    DBHandler dbHandler = new DBHandler();
+
+    public void setEmpty(TextField c, TextField fname, TextField lname, TextField city, TextField pnumber,
+                         TextField email) {
+        c.setText("");
+        fname.setText("");
+        lname.setText("");
+        city.setText("");
+        pnumber.setText("");
+        email.setText("");
+        datePicker = new DatePicker(LocalDate.now());
     }
-    
-	public void start(Stage stage)
-	{
-		stage.setTitle("Customer Registration");
-		
-		Font f=Font.font("Arial",FontWeight.NORMAL,FontPosture.REGULAR,15);
-		
-		add=new Button("New");
-		delete=new Button("Delete");
-		update=new Button("Update");
-		save=new Button("Save");
-		search=new Button("Search");
-		
-		add.setPrefSize(80,30);    add.setFont(f);
-		delete.setPrefSize(80,30); delete.setFont(f);
-		update.setPrefSize(80,30); update.setFont(f);
-		save.setPrefSize(80,30);   save.setFont(f);
-		search.setPrefSize(70,30); search.setFont(f);
-		
-		add.setOnAction(a->
-		{
-			setEmpty(tcid,tfname,tlname,tcity,tpnumber,temail,dp);
-		});
-		
-		save.setOnAction(a->
-		{ 
-			try
-			{
-				db.insertion(tfname,tlname,tcity,tpnumber,temail,dp);
-			}
-			catch(SQLException e) {}
-			
-			setEmpty(tcid,tfname,tlname,tcity,tpnumber,temail,dp);
-		});
-		
-		delete.setOnAction(a->
-		{
-			try
-			{
-				db.delete(tcid);
-			}
-		   catch (Exception e) {e.printStackTrace();} 
-			
-			setEmpty(tcid,tfname,tlname,tcity,tpnumber,temail,dp);
-		});
-		
-		search.setOnAction(a->
-		{
-			try
-			{
-				db.search(tcid,tfname,tlname,tcity,tpnumber,temail,dp);
-			}
-			catch(Exception e) {e.printStackTrace();}
-			
-		});
-		
-		update.setOnAction(a->
-		{
-			try
-			{
-				db.update(tcid,tfname,tlname,tcity,tpnumber,temail,dp);
-			}
-			catch(Exception e) {}
-			
-			setEmpty(tcid,tfname,tlname,tcity,tpnumber,temail,dp);
-		});
-		
-		VBox vb1=new VBox();
-		vb1.getChildren().addAll(add,delete,update,save);
-		vb1.setPadding(new Insets(30,30,0,0));
-		vb1.setSpacing(8);
-		vb1.setAlignment(Pos.TOP_RIGHT);
-		
-		cid=new Label("Customer ID"); cid.setFont(f);
-		tcid=new TextField();		  tcid.setFont(f);
-		tcid.setPrefSize(220,30);
-		
-		HBox hb1=new HBox();
-		hb1.getChildren().addAll(cid,tcid,search);
-		hb1.setPadding(new Insets(30,0,20,60));
-		hb1.setSpacing(10);
-		hb1.setAlignment(Pos.TOP_LEFT);
-		
-		fname=new Label("First Name"); fname.setFont(f);
-		tfname=new TextField(); 	   tfname.setFont(f);
-		tfname.setPrefSize(300,30);
-		
-		HBox hb2=new HBox();
-		hb2.getChildren().addAll(fname,tfname);
-		hb2.setPadding(new Insets(0,0,20,70));
-		hb2.setSpacing(10);
-		hb2.setAlignment(Pos.TOP_LEFT);
-		
-		lname=new Label("Last Name"); lname.setFont(f);
-		tlname=new TextField(); 	  tlname.setFont(f);
-		tlname.setPrefSize(300,30);
-		
-		HBox hb3=new HBox();
-		hb3.getChildren().addAll(lname,tlname);
-		hb3.setPadding(new Insets(0,0,20,70));
-		hb3.setSpacing(10);
-		hb3.setAlignment(Pos.TOP_LEFT);
-		
-		city=new Label("City"); 		  city.setFont(f);
-		tcity=new TextField();			  tcity.setFont(f);
-		tcity.setPrefSize(300,30);
-		
-		HBox hb4=new HBox();
-		hb4.getChildren().addAll(city,tcity);
-		hb4.setPadding(new Insets(0,0,20,115));
-		hb4.setSpacing(10);
-		hb4.setAlignment(Pos.TOP_LEFT);
-		
-		pnumber=new Label("Phone Number");  pnumber.setFont(f);
-		tpnumber=new TextField();			tpnumber.setFont(f);
-		tpnumber.setPrefSize(300,30);
-		
-		HBox hb5=new HBox();
-		hb5.getChildren().addAll(pnumber,tpnumber);
-		hb5.setPadding(new Insets(0,0,20,40));
-		hb5.setSpacing(10);
-		hb5.setAlignment(Pos.TOP_LEFT);
-		
-		email=new Label("Email");		email.setFont(f);
-		temail=new TextField();			temail.setFont(f);
-		temail.setPrefSize(300,30);
-		
-		HBox hb6=new HBox();
-		hb6.getChildren().addAll(email,temail);
-		hb6.setPadding(new Insets(0,0,20,105));
-		hb6.setSpacing(10);
-		hb6.setAlignment(Pos.TOP_LEFT);
-		
-		dateofreg=new Label("Registration Date"); dateofreg.setFont(f);
-		tdateofreg=new TextField();				  tdateofreg.setFont(f);
-		
-	    dp.getValue();
-		dp.setOnAction(a->{ tdateofreg.setText(""+dp.getValue()) ;});
-		
-		HBox hb7=new HBox();
-		hb7.getChildren().addAll(dateofreg,dp);
-		hb7.setPadding(new Insets(0,0,20,25));
-		hb7.setSpacing(10);
-		hb7.setAlignment(Pos.TOP_LEFT);
-		
-		VBox dataPart=new VBox();
-		dataPart.getChildren().addAll(hb1,hb2,hb3,hb4,hb5,hb6,hb7);
-		dataPart.setAlignment(Pos.TOP_LEFT);
-		
-		HBox complete=new HBox();
-		complete.getChildren().addAll(dataPart,vb1);
-		complete.setSpacing(100);
-		
-		BorderPane bp=new BorderPane(complete);
-		Scene sc=new Scene(bp,700,600);
-		stage.setScene(sc);
-		stage.show();
-		
-	}
-	
-	public static void main(String[] args) 
-	{
-		launch(args);
-	}
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Customer Registration");
+
+        Button add = new Button("New");
+        Button delete = new Button("Delete");
+        Button update = new Button("Update");
+        Button save = new Button("Save");
+        Button search = new Button("Search");
+
+        setButtonBehaviour(add, delete, update, save, search);
+        setFontAndPrefSize(add, delete, update, save, search);
+
+        VBox dataPart = new VBox();
+        dataPart.getChildren().addAll(
+                getCustomerIdHBox(search),
+                getFirstNameHBox(),
+                getLastNameHBox(),
+                getCityHBox(),
+                getPhoneNumberHBox(),
+                getEmailHBox(),
+                getDateHBox()
+        );
+
+        dataPart.setAlignment(Pos.TOP_LEFT);
+
+        HBox complete = new HBox();
+        complete.getChildren().addAll(dataPart, getVBox(add, delete, update, save));
+        complete.setSpacing(100);
+
+        BorderPane bp = new BorderPane(complete);
+        Scene sc = new Scene(bp, 700, 600);
+        stage.setScene(sc);
+        stage.show();
+    }
+
+    private void setButtonBehaviour(Button add, Button delete, Button update, Button save, Button search) {
+        add.setOnAction(a ->
+                setEmpty(tcid, tfname, tlname, tcity, tpnumber, temail));
+
+        save.setOnAction(a ->
+        {
+            try {
+                dbHandler.insertion(tfname, tlname, tcity, tpnumber, temail, datePicker);
+            } catch (SQLException ignored) {
+            }
+
+            setEmpty(tcid, tfname, tlname, tcity, tpnumber, temail);
+        });
+
+        delete.setOnAction(a ->
+        {
+            try {
+                dbHandler.delete(tcid);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            setEmpty(tcid, tfname, tlname, tcity, tpnumber, temail);
+        });
+
+        search.setOnAction(a ->
+        {
+            try {
+                dbHandler.search(tcid, tfname, tlname, tcity, tpnumber, temail);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        update.setOnAction(a ->
+        {
+            try {
+                dbHandler.update(tcid, tfname, tlname, tcity, tpnumber, temail, datePicker);
+            } catch (Exception ignored) {
+            }
+
+            setEmpty(tcid, tfname, tlname, tcity, tpnumber, temail);
+        });
+    }
+
+    private void setFontAndPrefSize(Button add, Button delete, Button update, Button save, Button search) {
+        add.setPrefSize(80, 30);
+        add.setFont(FONT);
+        delete.setPrefSize(80, 30);
+        delete.setFont(FONT);
+        update.setPrefSize(80, 30);
+        update.setFont(FONT);
+        save.setPrefSize(80, 30);
+        save.setFont(FONT);
+        search.setPrefSize(70, 30);
+        search.setFont(FONT);
+    }
+
+    private VBox getVBox(Button add, Button delete, Button update, Button save) {
+        VBox vb1 = new VBox();
+        vb1.getChildren().addAll(add, delete, update, save);
+        vb1.setPadding(new Insets(30, 30, 0, 0));
+        vb1.setSpacing(8);
+        vb1.setAlignment(Pos.TOP_RIGHT);
+        return vb1;
+    }
+
+    private HBox getCustomerIdHBox(Button search) {
+        Label cid = new Label("Customer ID");
+        cid.setFont(FONT);
+        tcid = new TextField();
+        tcid.setFont(FONT);
+        tcid.setPrefSize(220, 30);
+
+        HBox hb1 = new HBox();
+        hb1.getChildren().addAll(cid, tcid, search);
+        hb1.setPadding(new Insets(30, 0, 20, 60));
+        hb1.setSpacing(10);
+        hb1.setAlignment(Pos.TOP_LEFT);
+        return hb1;
+    }
+
+    private HBox getFirstNameHBox() {
+        Label fname = new Label("First Name");
+        fname.setFont(FONT);
+        tfname = new TextField();
+        tfname.setFont(FONT);
+        tfname.setPrefSize(300, 30);
+
+        HBox hb2 = new HBox();
+        hb2.getChildren().addAll(fname, tfname);
+        hb2.setPadding(new Insets(0, 0, 20, 70));
+        hb2.setSpacing(10);
+        hb2.setAlignment(Pos.TOP_LEFT);
+        return hb2;
+    }
+
+    private HBox getLastNameHBox() {
+        Label lname = new Label("Last Name");
+        lname.setFont(FONT);
+        tlname = new TextField();
+        tlname.setFont(FONT);
+        tlname.setPrefSize(300, 30);
+
+        HBox hb3 = new HBox();
+        hb3.getChildren().addAll(lname, tlname);
+        hb3.setPadding(new Insets(0, 0, 20, 70));
+        hb3.setSpacing(10);
+        hb3.setAlignment(Pos.TOP_LEFT);
+        return hb3;
+    }
+
+    private HBox getCityHBox() {
+        Label city = new Label("City");
+        city.setFont(FONT);
+        tcity = new TextField();
+        tcity.setFont(FONT);
+        tcity.setPrefSize(300, 30);
+
+        HBox hb4 = new HBox();
+        hb4.getChildren().addAll(city, tcity);
+        hb4.setPadding(new Insets(0, 0, 20, 115));
+        hb4.setSpacing(10);
+        hb4.setAlignment(Pos.TOP_LEFT);
+        return hb4;
+    }
+
+    private HBox getPhoneNumberHBox() {
+        Label pnumber = new Label("Phone Number");
+        pnumber.setFont(FONT);
+        tpnumber = new TextField();
+        tpnumber.setFont(FONT);
+        tpnumber.setPrefSize(300, 30);
+
+        HBox hb5 = new HBox();
+        hb5.getChildren().addAll(pnumber, tpnumber);
+        hb5.setPadding(new Insets(0, 0, 20, 40));
+        hb5.setSpacing(10);
+        hb5.setAlignment(Pos.TOP_LEFT);
+        return hb5;
+    }
+
+    private HBox getEmailHBox() {
+        Label email = new Label("Email");
+        email.setFont(FONT);
+        temail = new TextField();
+        temail.setFont(FONT);
+        temail.setPrefSize(300, 30);
+
+        HBox hb6 = new HBox();
+        hb6.getChildren().addAll(email, temail);
+        hb6.setPadding(new Insets(0, 0, 20, 105));
+        hb6.setSpacing(10);
+        hb6.setAlignment(Pos.TOP_LEFT);
+        return hb6;
+    }
+
+    private HBox getDateHBox() {
+        Label dateofreg = new Label("Registration Date");
+        dateofreg.setFont(FONT);
+        tdateofreg = new TextField();
+        tdateofreg.setFont(FONT);
+
+        datePicker.getValue();
+        datePicker.setOnAction(a -> {
+            tdateofreg.setText("" + datePicker.getValue());
+        });
+
+        HBox hb7 = new HBox();
+        hb7.getChildren().addAll(dateofreg, datePicker);
+        hb7.setPadding(new Insets(0, 0, 20, 25));
+        hb7.setSpacing(10);
+        hb7.setAlignment(Pos.TOP_LEFT);
+        return hb7;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 }
